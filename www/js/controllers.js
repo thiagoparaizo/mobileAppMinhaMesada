cb970390afda2f913bfd87d5d0eb17fde73994bc
thiagoparaizo <thiagoparaizo@tparaizo>
2016-10-25 00:42:41 -0300

    de casa

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,12 +3,27 @@ angular.module('starter.controllers', [])
 
 .controller('LoginCtrl', function($scope, $rootScope, LoginService, $ionicPopup, $state) {
 	$scope.showLoaderLogin = false;
+	$scope.showLoaderLoginFacebook = false;
+	$scope.showLoaderLoginGoogle = false;
+	
 	//função para simular href nos botoes
 	$scope.go = function ( state ) {
 		$state.go(state);
 	};
 	
 	
+	$scope.gravarLoginCredentias = function(usuario, token){
+		// User is signed in
+    	localStorage.setItem("minhamesadaappuserProfile", JSON.stringify(usuario));
+    	var userProfileLocal = localStorage.getItem("minhamesadaappuserProfile");
+    	$rootScope.userProfile = JSON.parse(userProfileLocal);
+    	
+    	if(token!=null){
+    		localStorage.setItem("minhamesadaappuserProfileToken", JSON.stringify(token));
+        	var userProfileTokenLocal = localStorage.getItem("minhamesadaappuserProfileToken");
+        	$rootScope.userProfileTokenLocal = JSON.parse(userProfileTokenLocal);
+    	}
+	}
 	
 	$scope.login = function(user){
 		
@@ -23,10 +38,7 @@ angular.module('starter.controllers', [])
 					  //3	var userFirebase = firebase.auth().currentUser;
 			            if (userFirebase != null) {
 			                // User is signed in
-			            	localStorage.setItem("minhamesadaappuserProfile", JSON.stringify(userFirebase));
-			            	var userProfileLocal = localStorage.getItem("minhamesadaappuserProfile");
-			            	$rootScope.userProfile = JSON.parse(userProfileLocal); 
-			            	
+			            	$scope.gravarLoginCredentias(userFirebase, null);
 			            	
 			            	console.log('userFirebase '+ userFirebase);//TODO resolver
 			            	console.log('$rootScope.userProfile '+ $rootScope.userProfile);//TODO resolver
@@ -77,11 +89,18 @@ angular.module('starter.controllers', [])
         });  
 	  }
 	
-	$scope.logout = function(user){
+	$rootScope.logout = function(){
 		firebase.auth().signOut().then(function() {
 			  // Sign-out successful.
-			localStorage.setItem("minhamesadaapp", null);
+			localStorage.setItem("minhamesadaappuserProfile", null);
+			localStorage.setItem("minhamesadaappuserProfileToken", null);
 			console.log('usuário deslogado!');
+			var alertPopup = $ionicPopup.alert({
+                title: 'Até mais ;)',
+	                template: 'Obrigado, nos vemos em breve!'
+	            });
+				
+				$state.go('login');
 			}, function(error) {
 			  // An error happened.
 			});
@@ -113,8 +132,19 @@ angular.module('starter.controllers', [])
                 template: 'As senhas digitadas são diferentes. Tente novamente. '
             });
 		}else{
-			firebase.auth().createUserWithEmailAndPassword(user.email, user.password1).catch(function(error) {
-				  // Handle Errors here.
+			firebase.auth().createUserWithEmailAndPassword(user.email, user.password1).then(function(user) {
+			    var user = firebase.auth().currentUser;
+			    $scope.gravarLoginCredentias(user, null);
+			    console.log('usuario criado...');
+			    
+			    var alertPopup = $ionicPopup.alert({
+	                title: 'Bem Vindo :)',
+	                template: 'Conta criada com sucesso! Seja bem vindo!'
+	            });
+			    $state.go('tab.dash');
+			    
+			}, function(error) {
+				// Handle Errors here.
 				  var errorCode = error.code;
 				  var errorMessage = error.message;
 				  
@@ -122,17 +152,88 @@ angular.module('starter.controllers', [])
 		                title: 'Ops :(',
 		                template: 'Não foi possivel concluir o seu cadastro. Detalhes: ['+errorCode+'] '+errorMessage
 		            });
-				  // ...
-				});
+			});
 		}
 		
 	}
 	
-	$scope.createUserShow = function(){
-		$scope.isCreateUserShow = true;
-	}
-	$scope.createUserHide = function(){
-		$scope.isCreateUserShow = false;
+	//$scope.createUserShow = function(){
+	//	$scope.isCreateUserShow = true;
+	//}
+	
+	//$scope.createUserHide = function(){
+	//	$scope.isCreateUserShow = false;
+	//}
+	
+	$scope.loginFacebook = function(){
+		$scope.showLoaderLoginFacebook = true;
+		console.log('Facebook login...0');
+		
+		var provider = new firebase.auth.FacebookAuthProvider();
+		provider.addScope('user_birthday');
+		provider.addScope('user_friends');
+		provider.addScope('email');
+		
+		//firebase.auth().signInWithRedirect(provider); //MOBILE - não funciona em desenvolvimento
+		//firebase.auth().getRedirectResult().then(function(result) {  //MOBILE - não funciona em desenvolvimento
+		
+		firebase.auth().signInWithPopup(provider).then(function(result) {  // DEVELOPMENT USE
+			var token = null;
+			if (result.credential) {
+				// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+				token = result.credential.accessToken;
+		  }
+		  // The signed-in user info.
+		  var user = result.user;
+		  console.log('Facebook login...1');
+		  
+		  if(user!=null){
+			  console.log('Facebook login...2');
+			  $scope.showLoaderLoginFacebook = false;
+			  $scope.gravarLoginCredentias(user, null);
+			  $scope.showLoaderLogin = false;
+				
+			  var alertPopup = $ionicPopup.alert({
+                title: 'Bem vindo ;)',
+	                template: 'Tudo certo, vamos começar!'
+	            });
+				
+			  console.log('Facebook login...3');
+				loadInitInfoDatabase();
+				$state.go('tab.gerenciados');
+				
+				
+		  }
+		  
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		 
+		  console.log('catch Facebook login: '+ errorCode + ' errorMessage: '+errorMessage);
+		  
+		  $scope.showLoaderLoginFacebook = false;
+		  if('auth/account-exists-with-different-credential'===errorCode){
+			  var alertPopup = $ionicPopup.alert({
+	              title: 'Erro no Login :(',
+	              template: 'Já existe um e-mail cadastrado. Tente entrar com email e senha, ou use a opção \'Esqueci a senha\'.'
+	          });
+		  }else{
+			  var alertPopup = $ionicPopup.alert({
+	              title: 'Erro no Login :(',
+	              template: 'Não foi possível logar com as informações do Facebook. Tente novamente ou use outra forma de login. ('+errorCode + errorMessage+')'
+	          });
+		  }
+      	
+		  
+		});
+		
+		
+		
 	}
 	
 	loadInitInfoDatabase = function(){
@@ -198,7 +299,7 @@ angular.module('starter.controllers', [])
   $scope.userProfile = TesteService.get();
 })
 
-.controller('AccountCtrl', function($scope, $state) {
+.controller('AccountCtrl', function($scope, $rootScope, $state) {
 	
 	$scope.toggleGroup = function(group) {
 	    console.log(group);
